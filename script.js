4e149ac836f2f13fc01469bd9dcf6a173222599e
KristianMeier <85857919+KristianMeier@users.noreply.github.com>
2021-09-19 00:45:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "selskab" && password === "selskab") {
+        window.open("https://selskab.herokuapp.com");
+    } 
+    else if (username === "enkeltmand" && password === "enkeltmand") {
+        window.open("https://enkeltmand.herokuapp.com");
+    }
+        else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
